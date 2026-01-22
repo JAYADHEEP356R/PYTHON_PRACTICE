c895340cc9aab75cc8992ec5693599a86a7187a0
JAYADHEEP356R <jayadheep.r@diggibyte.com>
2026-01-22 21:35:16 +0530

    variables

---
+++ src/variables.py
@@ -0,0 +1,33 @@
+# Variable declaration
+name = "Jayadheep"
+age = 21
+height = 5.8
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print("\n--- Multiple assignment ---")
+x, y, z = 10, 20, 30
+print(x, y, z)
+
+print("\n--- Same value assignment ---")
+a = b = c = 100
+print(a, b, c)
+
+print("\n--- Variable reassignment ---")
+age = 22
+print(age)
+
+print("\n--- Dynamic typing ---")
+value = 10
+print(value, type(value))
+
+value = "Python"
+print(value, type(value))
+
+print("\n--- Variable using expression ---")
+total = x + y + z
+print(total)
